64bfcd72e07108e6b7f527bb099fd326ff0c71a3
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2012-04-27 16:30:41 +0200

    moved download to separate file

---
+++ lib/download.c
@@ -0,0 +1,60 @@
+/****************************************************************************
+*
+* Copyright (C) 2011-2012  Adrian Solumsmo
+* 
+* imdb-plus program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* imdb-plus program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*****************************************************************************/
+
+#include <stdlib.h>
+#include <curl/curl.h>
+#include "download.h"
+
+
+/**
+ * download web pages from internet..
+ * @param char *url       url to read from
+ * @param char *saveas    name of file to save as
+ * @return                NULL
+ */
+void *download(void *download) {
+
+    CURL *curl;
+    CURLcode curl_res;
+
+    struct download *down = (struct download *)download;
+    
+    FILE *tmp = fopen(down->saveas, "w");
+
+    if(tmp == NULL) {
+        return NULL;
+    }
+
+    curl = curl_easy_init();
+
+    if(!curl) {
+        return NULL;
+    }
+
+    curl_easy_setopt(curl, CURLOPT_URL, down->url);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, tmp);
+    
+    curl_res = curl_easy_perform(curl);
+
+    fclose(tmp);
+    curl_easy_cleanup(curl);
+
+    return NULL;
+}
+
