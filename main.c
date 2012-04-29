10535d45519770d61d8af73a5eea4bf9ddc4d9e8
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2012-04-29 14:15:10 +0200

    fixed download dialog

--- main.c
+++ main.c
@@ -269,7 +269,7 @@ void menu_signal_update(GtkWidget *widget, gpointer data) {
     gtk_container_add(GTK_CONTAINER(content), label);
     gtk_container_add(GTK_CONTAINER(content), pbar);
 
-    gtk_container_set_border_width(GTK_CONTAINER(content->parent), 30);
+    gtk_container_set_border_width(GTK_CONTAINER(content->parent), 20);
     gtk_window_set_icon_from_file(GTK_WINDOW(dialog), APP_ICON, NULL);
     gtk_widget_show_all(GTK_WIDGET(dialog));
 
@@ -277,10 +277,10 @@ void menu_signal_update(GtkWidget *widget, gpointer data) {
 
     GThread *thread1, *thread2, *thread3;
 
-    /* someone press OK.. download some files */
+    /* someone pressed OK.. download some files */
     if(ret == GTK_RESPONSE_ACCEPT) {
 
-        gtk_label_set_text(GTK_LABEL(label), "Downloading lists..");
+        gtk_label_set_text(GTK_LABEL(label), "Downloading lists..\n");
 
         /* begin thread 1 */
         gtk_progress_bar_set_text(GTK_PROGRESS_BAR(pbar), "Top 250");
@@ -622,6 +622,107 @@ int menu_open_ratings(char *filename) {
 }
 
 
+GtkAdjustment *adj;
+GtkWidget *pbar, *label;
+
+/* download new ratings file response handler */
+void menu_signal_new_response(GtkWidget *dialog, int response, gpointer *data) {
+
+    /* destroy dialog on these events */
+    if((response == GTK_RESPONSE_CANCEL) || 
+       (response == GTK_RESPONSE_DELETE_EVENT)) {
+        gtk_widget_destroy(dialog);
+        return;
+    }
+
+    /* copy entered text */
+    char entered[100];
+    strcpy(entered, gtk_entry_get_text(GTK_ENTRY(entry)));
+
+    /* number should presumably be larger or smaller than this */
+    int enter_len = strlen(entered);
+
+    /* check input for length */
+    if(enter_len <= 6 || enter_len >= 20) {
+        gtk_label_set_text(GTK_LABEL(label), 
+            "Text should be 6 to 20 digits..\n");
+        return;
+    }
+
+    /* download from this url.. */
+    char load[100];
+    strcpy(load, CONST_URL);
+    strcat(load, entered);
+
+    /* save to this path.. */
+    char save[100];
+    strcpy(save, CONST_PREFIX);
+    strcat(save, entered);
+    strcat(save, ".csv");
+
+    /* set this progressbar message */
+    char pbar_text[50];
+    strcpy(pbar_text, "Downloading: ");
+    strcat(pbar_text, entered);
+    strcat(pbar_text, ".csv..");
+
+    gtk_label_set_text(GTK_LABEL(label), "Downloading ratings..\n");
+    gtk_progress_bar_set_text(GTK_PROGRESS_BAR(pbar), pbar_text);
+    gtk_adjustment_set_value(adj, 20);
+    while(gtk_events_pending()) gtk_main_iteration();
+
+    /* download new file */
+    struct download *dl = malloc(sizeof (struct download));
+    dl->url = load;
+    dl->saveas = save;
+
+    GThread *thread = g_thread_create(&download, dl, TRUE, NULL);
+
+    if(thread == 0) {
+        g_warning("can't create the thread");
+    }
+
+    g_thread_join(thread);
+
+    free(dl);
+
+    char *temp = NULL;
+    char *stat = NULL;
+    char *info = NULL;
+    int adjustment = 0;
+
+    /* open fresh ratings */
+    if(menu_open_ratings(save)) {
+
+        stat = "Opened ratings file: ";
+        info = "finished";
+        adjustment = 120;
+    }
+    else {
+        
+        stat = "Could not open file: ";
+        info = "failed..";
+        adjustment = 5;
+              
+        remove(save);
+    }
+
+
+    gtk_label_set_text(GTK_LABEL(label), 
+        "No dice. Is list set as public?\n");
+    gtk_progress_bar_set_text(GTK_PROGRESS_BAR(pbar), info);
+    gtk_adjustment_set_value(adj, adjustment);
+
+    temp = malloc(strlen(stat) + strlen(save) + 1);
+    strcpy(temp, stat),
+    strcat(temp, save);
+        
+    gtk_statusbar_push(GTK_STATUSBAR(status), 1, temp);
+    while(gtk_events_pending()) gtk_main_iteration();
+
+    free(temp);
+}
+
 /* open new ratings download dialog */
 void menu_signal_new(GtkWidget *widget, gpointer data) {
     
@@ -630,124 +731,40 @@ void menu_signal_new(GtkWidget *widget, gpointer data) {
         GTK_WINDOW(widget->parent), 
         GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT, 
         GTK_STOCK_OK, 
-        GTK_RESPONSE_ACCEPT, 
+        GTK_RESPONSE_OK, 
         GTK_STOCK_CANCEL, 
-        GTK_RESPONSE_REJECT, 
+        GTK_RESPONSE_CANCEL, 
         NULL
     );
     
-    GtkAdjustment *adj = (GtkAdjustment*)gtk_adjustment_new(5, 0, 120, 0, 0, 0);
+    adj = (GtkAdjustment*)gtk_adjustment_new(5, 0, 120, 0, 0, 0);
     GtkWidget *vbox = gtk_vbox_new(FALSE, 5);
 
-    GtkWidget *pbar = gtk_progress_bar_new_with_adjustment(adj);
+    pbar = gtk_progress_bar_new_with_adjustment(adj);
     gtk_progress_bar_set_text(GTK_PROGRESS_BAR(pbar), "Waiting..");
+
+    label = gtk_label_new("Enter IMDb ID i.e. 4854451\n");
 
     gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_CENTER);
 
     content = gtk_dialog_get_content_area(GTK_DIALOG(dialog));
     entry = gtk_entry_new();
 
-    GtkWidget *label = gtk_label_new(" Enter unique IMDb ID i.e. 4854451\n");
     gtk_container_add(GTK_CONTAINER(content), label);
 
     gtk_box_pack_start(GTK_BOX(vbox), entry, FALSE, FALSE, 0);
     gtk_box_pack_start(GTK_BOX(vbox), pbar, TRUE, TRUE, 0);
 
     gtk_container_add(GTK_CONTAINER(content), vbox);
+    
+    g_signal_connect(dialog, "response", 
+        G_CALLBACK(menu_signal_new_response), NULL);
 
-    gtk_container_set_border_width(GTK_CONTAINER(content->parent), 30);
+    gtk_container_set_border_width(GTK_CONTAINER(content->parent), 20);
     gtk_window_set_icon_from_file(GTK_WINDOW(dialog), APP_ICON, NULL);
     gtk_widget_show_all(GTK_WIDGET(dialog));
 
-    /* run dialog */
-    if(gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
-
-        /* copy entered text */
-        char entered[100];
-        strcpy(entered, gtk_entry_get_text(GTK_ENTRY(entry)));
-
-        /* number should presumably be larger or smaller than this */
-        int enter_len = strlen(entered);
-
-        if(enter_len <= 6 || enter_len >= 20) {
-            return;
-        }
-
-        /* download from this url.. */
-        char load[100];
-        strcpy(load, CONST_URL);
-        strcat(load, entered);
-
-        /* save to this path.. */
-        char save[100];
-        strcpy(save, CONST_PREFIX);
-        strcat(save, entered);
-        strcat(save, ".csv");
-
-        /* set this progressbar message */
-        char pbar_text[50];
-        strcpy(pbar_text, "Downloading: ");
-        strcat(pbar_text, entered);
-        strcat(pbar_text, ".csv..");
-
-        gtk_label_set_text(GTK_LABEL(label), "Downloading ratings..");
-        gtk_progress_bar_set_text(GTK_PROGRESS_BAR(pbar), pbar_text);
-        gtk_adjustment_set_value(adj, 20);
-        while(gtk_events_pending()) gtk_main_iteration();
-
-        /* download new file */
-        struct download *dl = malloc(sizeof (struct download));
-        dl->url = load;
-        dl->saveas = save;
-
-        GThread *thread = g_thread_create(&download, dl, TRUE, NULL);
-
-        if(thread == 0) {
-            g_warning("can't create the thread");
-        }
-
-        g_thread_join(thread);
-
-        free(dl);
-
-        char *temp = NULL;
-        char *stat = NULL;
-        char *info = NULL;
-        int adjustment = 0;
-
-        /* open fresh ratings */
-        if(menu_open_ratings(save)) {
-
-            stat = "Opened ratings file: ";
-            info = "finished";
-            adjustment = 120;
-        }
-        else {
-            
-            stat = "Could not open file: ";
-            info = "Waiting..";
-            adjustment = 5;
-                  
-            remove(save);
-        }
-
-        gtk_progress_bar_set_text(GTK_PROGRESS_BAR(pbar), info);
-        gtk_adjustment_set_value(adj, adjustment);
-
-        temp = malloc(strlen(stat) + strlen(save) + 1);
-        strcpy(temp, stat),
-        strcat(temp, save);
-            
-        gtk_statusbar_push(GTK_STATUSBAR(status), 1, temp);
-
-        while(gtk_events_pending()) gtk_main_iteration();
-
-        gtk_widget_destroy(dialog);
-    }
-        
-    if(GTK_IS_WIDGET(dialog)) {
-        gtk_widget_destroy(dialog);
-    }
+    gtk_dialog_run(GTK_DIALOG(dialog));
 }
 
 
