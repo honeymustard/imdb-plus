96ffc159616547e6bdc372e922e47bf599860967
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2012-05-12 22:43:38 +0200

    moved independent color definitions to lib/colors.c

--- main.c
+++ main.c
@@ -25,31 +25,9 @@
 #include "master.h"
 #include "tables.h"
 #include "widgets.h"
+#include "lib/colors.h"
 #include "lib/events/events.h"
 #include "lib/gtk_custom_table/gtk_custom_table.h"
-
-
-/* solid green to solid red, for ratings */
-double colors[10][3] = {
-    {1.00, 0.00, 0.00}, 
-    {1.00, 0.15, 0.00}, 
-    {1.00, 0.35, 0.00}, 
-    {1.00, 0.55, 0.00}, 
-    {1.00, 0.75, 0.00}, 
-    {1.00, 0.95, 0.00}, 
-    {0.85, 1.00, 0.00}, 
-    {0.65, 1.00, 0.00}, 
-    {0.35, 1.00, 0.00}, 
-    {0.00, 1.00, 0.00}, 
-};
-
-
-/* graph background colors */
-double graph_bg[3] = {
-    0.90, 
-    0.90, 
-    0.90,
-};
 
 
 /**
