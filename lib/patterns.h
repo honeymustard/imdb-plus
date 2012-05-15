4df9a09f3988218b31ed10d84cadd8dfec9813b9
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2012-05-15 02:00:56 +0200

    moved regex patterns from regex.c to their own file

---
+++ lib/patterns.h
@@ -0,0 +1,31 @@
+/****************************************************************************
+*
+* Copyright (C) 2011-2012  Adrian Solumsmo
+* 
+* imdb-plus program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* imdb-plus program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*****************************************************************************/
+
+
+#ifndef _PATTERNS_
+#define _PATTERNS_
+
+/* regex patterns */
+char *pattern_top250;
+char *pattern_bot100;
+char *pattern_boxoffice;
+char *pattern_boxoffice_win;
+
+#endif
+
