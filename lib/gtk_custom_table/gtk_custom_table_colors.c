a68e19fd718e34d5ccfe5b21e7dd0bb448359d4f
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2012-10-16 05:03:04 +0200

    added new file to house various rgb definitions..

---
+++ lib/gtk_custom_table/gtk_custom_table_colors.c
@@ -0,0 +1,70 @@
+/****************************************************************************
+*
+* Copyright (C) 2011-2012  Adrian Solumsmo
+* 
+* imdb-plus program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* imdb-plus program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*****************************************************************************/
+
+
+#include "gtk_custom_table.h"
+
+
+double checkers[2][3] = {
+    {1.00, 1.00, 1.00}, 
+    {0.95, 0.95, 0.99}, 
+};
+
+double rgb_header[3] = {
+    0.8, 
+    0.8, 
+    0.8, 
+};
+
+double rgb_footer[3] = {
+    0.8, 
+    0.8, 
+    0.8, 
+};
+
+double rgb_header_bold[3] = {
+    0.87, 
+    0.90, 
+    0.99, 
+};
+
+double rgb_border[3] = {
+    0.2, 
+    0.2, 
+    0.2, 
+};
+
+double rgb_text[3] = {
+    0.1, 
+    0.1, 
+    0.1, 
+};
+
+double rgb_graph[] = {
+    0.00, 
+    0.90, 
+    0.00,
+};
+
+double rgb_cell[] = {
+    1.00, 
+    1.00, 
+    1.00,
+};
+
