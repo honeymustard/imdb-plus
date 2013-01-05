0dc4526e7baac38ad4c09a62ca96315a91496a6a
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2013-01-05 23:57:58 +0100

    changed the default column text alignment to right-aligned..

--- src/ui/ui_set/ui_set_meta.c
+++ src/ui/ui_set/ui_set_meta.c
@@ -61,146 +61,30 @@ void ui_set_meta() {
         TEXT_FONT); 
     gtk_custom_table_set_foot_cell_font(nb_tab_lists_stats, 3, 
         TEXT_FONT); 
+    gtk_custom_table_set_foot_cell_font(nb_tab_compare, 3, 
+        TEXT_FONT); 
  
     /* set font alignments */
-    gtk_custom_table_set_column_alignment(nb_tab_statistics, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_statistics, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_statistics, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_statistics, 4, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_statistics, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_statistics, 6, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_statistics, 7, 
-        PANGO_ALIGN_RIGHT);
+    gtk_custom_table_set_column_alignment(nb_tab_statistics, 3, 
+        PANGO_ALIGN_LEFT);
+    gtk_custom_table_set_column_alignment(nb_tab_lists_stats, 3, 
+        PANGO_ALIGN_LEFT);
+    gtk_custom_table_set_column_alignment(nb_tab_compare, 3, 
+        PANGO_ALIGN_LEFT);
+    gtk_custom_table_set_column_alignment(nb_tab_allstats, 3, 
+        PANGO_ALIGN_LEFT);
 
-    gtk_custom_table_set_column_alignment(nb_tab_lists_stats, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_stats, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_stats, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_stats, 4, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_stats, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_stats, 6, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_stats, 7, 
-        PANGO_ALIGN_RIGHT);
-
-    gtk_custom_table_set_column_alignment(nb_tab_compare, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_compare, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_compare, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_compare, 4, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_compare, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_compare, 6, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_compare, 7, 
-        PANGO_ALIGN_RIGHT);
-
-    gtk_custom_table_set_column_alignment(nb_tab_allstats, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_allstats, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_allstats, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_allstats, 4, 
-        PANGO_ALIGN_CENTER);
-    gtk_custom_table_set_column_alignment(nb_tab_allstats, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_allstats, 6, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_allstats, 7, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_allstats, 8, 
-        PANGO_ALIGN_RIGHT);
-
-    gtk_custom_table_set_column_alignment(nb_tab_top250, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_top250, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_top250, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_top250, 3, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_top250, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_top250, 6, 
-        PANGO_ALIGN_RIGHT);
-
-    gtk_custom_table_set_column_alignment(nb_tab_bot100, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_bot100, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_bot100, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_bot100, 3, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_bot100, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_bot100, 6, 
-        PANGO_ALIGN_RIGHT);
-
-    gtk_custom_table_set_column_alignment(nb_tab_boxoffice, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_boxoffice, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_boxoffice, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_boxoffice, 3, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_boxoffice, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_boxoffice, 6, 
-        PANGO_ALIGN_RIGHT);
-
-    gtk_custom_table_set_column_alignment(nb_tab_mymovies, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_mymovies, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_mymovies, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_mymovies, 3, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_mymovies, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_mymovies, 6, 
-        PANGO_ALIGN_RIGHT);
-
-    gtk_custom_table_set_column_alignment(nb_tab_lists, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists, 3, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists, 6, 
-        PANGO_ALIGN_RIGHT);
-
-    gtk_custom_table_set_column_alignment(nb_tab_lists_cmp, 0, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_cmp, 1, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_cmp, 2, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_cmp, 3, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_cmp, 5, 
-        PANGO_ALIGN_RIGHT);
-    gtk_custom_table_set_column_alignment(nb_tab_lists_cmp, 6, 
-        PANGO_ALIGN_RIGHT);
+    gtk_custom_table_set_column_alignment(nb_tab_top250, 4, 
+        PANGO_ALIGN_LEFT);
+    gtk_custom_table_set_column_alignment(nb_tab_bot100, 4, 
+        PANGO_ALIGN_LEFT);
+    gtk_custom_table_set_column_alignment(nb_tab_boxoffice, 4, 
+        PANGO_ALIGN_LEFT);
+    gtk_custom_table_set_column_alignment(nb_tab_mymovies, 4, 
+        PANGO_ALIGN_LEFT);
+    gtk_custom_table_set_column_alignment(nb_tab_lists, 4, 
+        PANGO_ALIGN_LEFT);
+    gtk_custom_table_set_column_alignment(nb_tab_lists_cmp, 4, 
+        PANGO_ALIGN_LEFT);
 }
 
